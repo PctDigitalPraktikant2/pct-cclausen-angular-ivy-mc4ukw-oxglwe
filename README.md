02f6aaea1eee199d5472b702e2086735632db759
PctDigitalPraktikant2 <pctdigitalpraktikant2@gmail.com>
2022-08-25 13:42:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pct-cclausen-angular-ivy-mc4ukw-oxglwe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pct-cclausen-angular-ivy-mc4ukw-oxglwe)
---
+++ src/app/login/pct-login/pct-login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { pctlogo } from '../../images';
+
+@Component({
+  selector: 'app-pct-login',
+  templateUrl: './pct-login.component.html',
+  styleUrls: ['./pct-login.component.css'],
+})
+export class PctLoginComponent implements OnInit {
+  userName = '';
+  pctlogo=pctlogo
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const cu = localStorage['current-user'];
+    if (cu != null && cu.length > 0) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  loginHandler() {
+    if (this.userName.length > 0) {
+      localStorage['current-user'] = this.userName;
+      this.router.navigate(['/']);
+    } else {
+      alert('Bitte Namen eingeben!');
+    }
+  }
+}
